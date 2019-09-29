1b75bcada1d1e1323b90d555bbe2849851d18000
Miller <chailunlun@superatomfin.com>
2019-09-29 19:16:04 +0800

    增减healthy和test兼容

--- example/health_client.go
+++ example/health_client.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	proto "cyclone/proto"
+	proto "cyclone/example/proto"
+	healthy "cyclone/healthy"
 	"fmt"
 	"github.com/micro/go-micro"
 
@@ -14,9 +15,9 @@ import (
 var (
 	FuserName = "Banner.Carousel"
 )
-var iClient proto.HealthyService
+var iClient healthy.CycloneHealthyService
 
-func GetClient() (proto.HealthyService, error) {
+func GetHealthyClient() (healthy.CycloneHealthyService, error) {
 	if iClient == nil {
 		reg, _ := cyclone.NewRegistry(&cyclone.RegistryConf{
 			Registry:        "consul",
@@ -27,14 +28,62 @@ func GetClient() (proto.HealthyService, error) {
 			micro.Registry(reg),
 		).Client()
 		srv.Init()
-		iClient = proto.NewHealthyService("test_healthy", srv)
+		iClient = healthy.NewCycloneHealthyService("test_healthy", srv)
 	}
 	return iClient, nil
 }
+
+
+var iiClient proto.CycloneService
+
+
+func GetCycloneClient() (proto.CycloneService, error) {
+	if iiClient == nil {
+
+		reg, _ := cyclone.NewRegistry(&cyclone.RegistryConf{
+			Registry:        "consul",
+			RegistryAddress: []string{"127.0.0.1:8500"},
+		})
+
+		srv := cli.NewService(
+			micro.Registry(reg),
+		).Client()
+		srv.Init()
+		iiClient = proto.NewCycloneService("test_healthy", srv)
+	}
+	return iiClient, nil
+}
 func main() {
-	iCli, _ := GetClient()
+
+	x := &cyclone.MonitorConfig{
+		Registry: &cyclone.RegistryConf{"consul", []string{"127.0.0.1:8500"}},
+		Type:     cyclone.MonitorTypeAddress,
+		Services: []*cyclone.SrvConfigInfo{
+			{
+				Name:  "go.micro.util.srv.zipcode",
+				Hosts: []string{"127.0.0.1:54901"},
+			},
+		},
+		Match: cyclone.MatchTypeFull,
+	}
+	m, _ := cyclone.NewMonitor("TestMonitorAddressFull", x)
+	fmt.Println(m.Run())
+
+	iCli, _ := GetHealthyClient()
 
 	innerRes, innerErr := iCli.Healthy(
+		context.Background(),
+		&healthy.CycloneRequest{
+		},
+
+		//func(option *client.CallOptions) {
+		//	option.Address = []string{"10.xxx.xxx.15:63372"}
+		//},
+	)
+	fmt.Println(innerRes, innerErr)
+	iiCli, _ := GetCycloneClient()
+
+	iinnerRes, iinnerErr := iiCli.Cyclone(
 		context.Background(),
 		&proto.Request{
 		},
@@ -43,6 +92,6 @@ func main() {
 		//	option.Address = []string{"10.xxx.xxx.15:63372"}
 		//},
 	)
-	fmt.Println(innerRes, innerErr)
+	fmt.Println("xxxxxx",iinnerRes, iinnerErr)
 
 }
--- example/health_server.go
+++ example/health_server.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"cyclone"
-	proto "cyclone/proto"
+	proto "cyclone/example/proto"
+	healthy "cyclone/healthy"
 	"fmt"
 	"github.com/micro/go-grpc"
 	"github.com/micro/go-micro"
@@ -12,17 +13,37 @@ import (
 type testHealthyHandler struct {
 }
 
-func (hh testHealthyHandler) Healthy(ctx context.Context, req *proto.Request, res *proto.Response) error {
-	res.Msg = "SUCCESS"
-	res.Code = proto.Response_Healthy
+func (hh testHealthyHandler) Healthy(ctx context.Context, req *healthy.CycloneRequest, res *healthy.CycloneResponse) error {
+	res.Response = &healthy.ServiceStatus{
+		Name: "healthy",
+		Api: []*healthy.ApiInfo{
+			{
+				Api:   "healthy",
+				Error: "",
+			},
+		},
+	}
+	res.Code = healthy.CycloneResponse_Healthy
 	return nil
 }
+
+
+
+type testCycloneHandler struct {
+}
+
+func (hh testCycloneHandler) Cyclone(ctx context.Context, req *proto.Request, res *proto.Response) error {
+	res.Code = proto.Response_Fail
+	return nil
+}
+
 
 func main() {
 	service := grpc.NewService(
 		micro.Name("test_healthy"),
 	)
 	_ = proto.RegisterHealthyHandler(service.Server(), &testHealthyHandler{})
+	_ = proto.RegisterCycloneHandler(service.Server(), &testCycloneHandler{})
 	srv, err := cyclone.NewServiceBuilder(service, nil, &cyclone.Setting{
 		Threshold: 5,  // 计数器阈值, 溢出后表服务不可用
 		Duration:  30, // 计数器统计时间周期, 距离当前多少秒内
